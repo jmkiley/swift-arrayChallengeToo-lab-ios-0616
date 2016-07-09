296a8a831b528e43f3e391db7210a164a1e45d15
jmkiley <jordankiley@gmail.com>
2016-07-09 17:32:48 -0400

    Done.

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -15,13 +15,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addNameToDeliLine("Meg Ryan")
+        nowServing()
+        deliLineDescription()
     }
 
    
     
     
     // Create your methods here
-
-
+    func addNameToDeliLine(name: String) -> String {
+        if name == "Billy Crystal"  {
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Billy! You can sit wherever you like."
+        } else if name == "Meg Ryan"{
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Meg! You can sit wherever you like."
+        } else  {
+            deliLine.append(name)
+            if deliLine.count == 1 {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(deliLine.count) in line."
+            }
+        }
+        
+    }
+    
+    func nowServing() -> String {
+        if deliLine.count == 0 {
+            return "There is no-one to be served."
+        } else {
+            let firstPerson = deliLine[0]
+            deliLine.removeFirst()
+            return "Now serving \(firstPerson)!"
+        }
+    }
+    
+    func deliLineDescription() -> String {
+        if deliLine.count == 0 {
+            return "The line is currently empty."
+        } else {
+            var lineContents = "The line is:"
+            for (index, person) in deliLine.enumerate() {
+                lineContents.appendContentsOf("\n\(index+1). \(person)")
+            }
+            return lineContents
+        }
+    }
 }
-
